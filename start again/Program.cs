296e8d2ce4d2e6e1099b60e6f0dd4584b2696bfa
eyad <eyad@MOHAMED>
2017-08-08 15:55:57 +0200

    add a comment

--- start again/Program.cs
+++ start again/Program.cs
@@ -10,6 +10,8 @@ namespace start_again
     {
         static void Main(string[] args)
         {
+            //this is my first program
+
             Console.WriteLine("Hello World");
             Console.ReadKey();
         }
